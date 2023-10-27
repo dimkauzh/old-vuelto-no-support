def3f19c8971d276c98394a1eccecf207589bea7
Dima Marhitych <uzhdimka@gmail.com>
2023-10-27 17:14:57 +0300

    Create example.go

---
+++ example/example.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/dimkauzh/vuelto"
+	"github.com/dimkauzh/vuelto/src/window"
+)
+
+func main() {
+	vuelto.Init()
+	win := window.NewWindow("Vuelto Renderer", 800, 600)
+
+	for win.Loop() {
+		win.DrawRect(100, 100, 100, 100, [3]int{0, 255, 0})
+		win.DrawRect(200, 300, 100, 100, [3]int{0, 255, 0})
+
+	}
+}
