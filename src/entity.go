bb9273086af0e700ad67752c1880b60c4567b888
Dima Marhitych <uzhdimka@gmail.com>
2023-10-30 18:56:46 +0100

    Go fmt

--- src/entity.go
+++ src/entity.go
@@ -1,22 +1,22 @@
 package src
 
 type Entity struct {
-    X      int
-    Y      int
-    Width  int
-    Height int
-    Image  *Image
-    Rect   *Rect
+	X      int
+	Y      int
+	Width  int
+	Height int
+	Image  *Image
+	Rect   *Rect
 }
 
 func (w *Window) NewEntity(x int, y int, width int, height int) Entity {
-    return Entity{x, y, width, height, nil, nil}
+	return Entity{x, y, width, height, nil, nil}
 }
 
 func (e *Entity) SetImage(image Image) {
-    e.Image = &image
+	e.Image = &image
 }
 
 func (e *Entity) SetRect(rect Rect) {
-    e.Rect = &rect
+	e.Rect = &rect
 }
--- src/vector2d.go
+++ src/vector2d.go
@@ -1,10 +1,10 @@
 package src
 
 type Vector2D struct {
-    x int
-    y int
+	x int
+	y int
 }
 
 func (w *Window) NewVector2D(x, y int) Vector2D {
-    return Vector2D{x, y}
+	return Vector2D{x, y}
 }
